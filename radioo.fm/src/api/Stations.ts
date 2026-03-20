82713e423ea184472b3b81dbc52c20bb34592c31
goncalo marques <goncalomarques112@gmail.com>
2026-03-20 09:55:19 +0000

    fix erropr 404

--- radioo.fm/src/api/Stations.ts
+++ radioo.fm/src/api/Stations.ts
@@ -1,12 +1,8 @@
 import type { Station } from "../types/station";
-
-// O prefixo '/radio-api' será interceptado pelo Vite (Local) ou pelo Vercel (Produção)
-const BASE_URL = "/radio-api"; 
+const BASE_URL = "https://de1.api.radio-browser.info/json";
 
 export async function getStationsByTag(tag: string): Promise<Station[]> {
-  // A URL final será: /radio-api/stations/bytag/lofi?limit=50
   const res = await fetch(`${BASE_URL}/stations/bytag/${tag}?limit=50`);
 
-  if (!res.ok) throw new Error("Falha ao carregar estações");
   return res.json();
 }
